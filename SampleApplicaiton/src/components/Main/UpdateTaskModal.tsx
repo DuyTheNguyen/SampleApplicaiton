38c6889e24e1c4cf1f8d1dd442224db5d5805280
The Deligent Dave <duy27295@gmail.com>
2019-09-21 16:09:39 +1000

    fix fetching bug

--- SampleApplicaiton/src/components/Main/UpdateTaskModal.tsx
+++ SampleApplicaiton/src/components/Main/UpdateTaskModal.tsx
@@ -3,7 +3,6 @@ import {Modal, Button, InputGroup, FormControl} from 'react-bootstrap';
 const PUT_API = location.protocol + '//' + location.host + '/api/task/';
 
 export interface Props{
-    name: string;
     onHide: () => void;
     reload: () => void;
     show: boolean;
@@ -27,6 +26,26 @@ class UpdateTaskModal extends React.Component<Props, State>{
     observingUpdateName = (e) => {this.setState({updateName: e.target.value})}
     observingUpdateDes = (e) => {this.setState({updateDes: e.target.value})}
 
+    updateTask = (id) => {
+        let task = {
+            "ID": id,
+            "name": this.state.updateName,
+            "description": this.state.updateDes
+        }
+        console.log(PUT_API+ id);
+        fetch(PUT_API + id, {
+            method:"PUT",
+            body: JSON.stringify(task),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .catch(error => console.log(error))
+        
+        this.props.reload();
+    }
+
     render(){
         const {updateName, updateDes} = this.state;
         const {onHide, name, taskId} = this.props;
@@ -39,7 +58,7 @@ class UpdateTaskModal extends React.Component<Props, State>{
             >
                 <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    {taskId}
+                    Update Task
                 </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
@@ -70,13 +89,13 @@ class UpdateTaskModal extends React.Component<Props, State>{
                 </Modal.Body>
                 <Modal.Footer>
                     <Button 
-                        variant="primary"
+                        variant="warning"
                         disabled={updateName == "" || updateDes == ""}
-                        
+                        onClick = {() => this.updateTask(taskId)}
                     >
                         {name} 
                     </Button>
-                    <Button variant="outline-primary" onClick={onHide}>Cancel</Button>
+                    <Button variant="outline-warning" onClick={onHide}>Cancel</Button>
                 </Modal.Footer>
             </Modal>
         );
